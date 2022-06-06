9d0d07086cb9371466c602a0a28bd212d9360c66
koviubi56 <koviubi56@protonmail.com>
2022-06-06 20:48:08 +0200

    build: :bookmark: Changed version

--- src/mylog/__init__.py
+++ src/mylog/__init__.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 import abc
 import contextlib
