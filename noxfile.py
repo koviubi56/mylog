90e2bf4051da3574141bde12d03ea85a65f64dc6
koviubi56 <koviubi56@duck.com>
2023-09-22 19:04:10 +0000

    test: ✅ Also test 3.12 in noxfile
    See [Call to action](https://www.python.org/downloads/release/python-3120rc3/)

--- noxfile.py
+++ noxfile.py
@@ -6,8 +6,7 @@ import nox
 # run `test`, because `test_coverage` also uploads to CodeCov
 nox.options.sessions = ["test"]
 
-# 3.12 not out yet
-PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
